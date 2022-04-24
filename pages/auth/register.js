d8cf5af05fd5dc7b81dcb63a71868e3b6fb097bb
josetantaxD <78164454+josetantaxD@users.noreply.github.com>
2022-04-24 15:34:41 -0500

    no subir24/4
    tarde

--- pages/auth/register.js
+++ pages/auth/register.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "assets/css/registro.css"
 
 // reactstrap components
 import {
@@ -17,50 +18,62 @@ import {
 } from "reactstrap";
 // layout for this page
 import Auth from "layouts/Auth.js";
+import AuthCustom from "layouts/AuthCustom";
 
 function Register() {
   return (
     <>
-      <Col lg="6" md="8">
+      <div className="text-registro">
+        <span>Para poder registrarte necesitas saber lo siguiente:</span>
+        <div className="box-registro">
+          <span>1</span><p>El Club de Ciencia y Tecnología tiene un nombre que lo identifica, deberá estar relacionada con temas cientificos y/o con nombres de científicos que hayan aportado el desarrollo del conocimiento. <br></br><b>Ejemplo: Albert Einsten</b></p>
+        </div>
+        <div className="box-registro">
+          <span>2</span><p>Que los correos registrados solo son válidos en GMAIL.<br></br>
+            <b>Ejemplo: ccytalberteinsten@gmail.com</b></p>
+        </div>
+      </div>
+      <Col lg="4" md="8">
         <Card className="bg-secondary shadow border-0">
           <CardHeader className="bg-transparent pb-5">
             <div className="text-muted text-center mt-2 mb-4">
-              <small>Sign up with</small>
+              <small></small>
             </div>
-            <div className="text-center">
-              <Button
-                className="btn-neutral btn-icon mr-4"
-                color="default"
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                <span className="btn-inner--icon">
-                  <img
+            <div className="btn-wrapper text-center">
+              <div className="logo-up">
+                <div className="box-img1">
+                  <img className="box-img-1"
                     alt="..."
-                    src={require("assets/img/icons/common/github.svg")}
+                    src={require("assets/img/icons/common/concytec.png")}
                   />
-                </span>
-                <span className="btn-inner--text">Github</span>
-              </Button>
-              <Button
-                className="btn-neutral btn-icon"
-                color="default"
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                <span className="btn-inner--icon">
-                  <img
+                </div>
+                <div className="box-img2">
+                  <img className="box-img-2"
                     alt="..."
-                    src={require("assets/img/icons/common/google.svg")}
+                    src={require("assets/img/icons/common/club.png")}
                   />
-                </span>
-                <span className="btn-inner--text">Google</span>
-              </Button>
+                </div>
+              </div>
+              <div className="box-img3">
+                <img className="box-img-3"
+                  alt="..."
+                  src={require("assets/img/icons/common/SIGECCYT.png")}
+                />
+              </div>
+              <div className="logo-righ">
+                <div className="box-img5">
+                  <img className="box-img-4"
+                    alt="..."
+                    src={require("assets/img/icons/home/IMG1.png")}
+                  />
+                  <span>CCYT</span>
+                </div>
+              </div>
             </div>
           </CardHeader>
           <CardBody className="px-lg-5 py-lg-5">
             <div className="text-center text-muted mb-4">
-              <small>Or sign up with credentials</small>
+              <small>Registro como CCYT</small>
             </div>
             <Form role="form">
               <FormGroup>
@@ -70,7 +83,7 @@ function Register() {
                       <i className="ni ni-hat-3" />
                     </InputGroupText>
                   </InputGroupAddon>
-                  <Input placeholder="Name" type="text" />
+                  <Input placeholder="Nombre del CCYT" type="text" />
                 </InputGroup>
               </FormGroup>
               <FormGroup>
@@ -81,7 +94,7 @@ function Register() {
                     </InputGroupText>
                   </InputGroupAddon>
                   <Input
-                    placeholder="Email"
+                    placeholder="Correo del CCYT"
                     type="email"
                     autoComplete="new-email"
                   />
@@ -95,18 +108,18 @@ function Register() {
                     </InputGroupText>
                   </InputGroupAddon>
                   <Input
-                    placeholder="Password"
+                    placeholder="Contraseña"
                     type="password"
                     autoComplete="new-password"
                   />
                 </InputGroup>
               </FormGroup>
-              <div className="text-muted font-italic">
+              {/* <div className="text-muted font-italic">
                 <small>
                   password strength:{" "}
                   <span className="text-success font-weight-700">strong</span>
                 </small>
-              </div>
+              </div> */}
               <Row className="my-4">
                 <Col xs="12">
                   <div className="custom-control custom-control-alternative custom-checkbox">
@@ -120,9 +133,9 @@ function Register() {
                       htmlFor="customCheckRegister"
                     >
                       <span className="text-muted">
-                        I agree with the{" "}
+                        Aceptar{" "}
                         <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                          Privacy Policy
+                          Términos y Condiciones
                         </a>
                       </span>
                     </label>
@@ -131,7 +144,7 @@ function Register() {
               </Row>
               <div className="text-center">
                 <Button className="mt-4" color="primary" type="button">
-                  Create account
+                  Crear cuenta
                 </Button>
               </div>
             </Form>
@@ -142,6 +155,6 @@ function Register() {
   );
 }
 
-Register.layout = Auth;
+Register.layout = AuthCustom;
 
 export default Register;

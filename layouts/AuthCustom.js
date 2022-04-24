5eb981cf63325658151fd7d0a0566a18c1c20d68
josetantaxD <78164454+josetantaxD@users.noreply.github.com>
2022-04-24 14:22:13 -0500

    no subir 24/4
    :D

--- layouts/AuthCustom.js
+++ layouts/AuthCustom.js
@@ -1,10 +1,13 @@
-import fondoHome from "assets/img/icons/home/diseño.png"
+// import fondoHome from "assets/img/icons/home/diseño.png"
 import "assets/css/home.css"
 
-function AuthCustom(props){
+function AuthCustom(props) {
     return <div className="conteiner-home">
-        <img src={fondoHome}/>
-        <label>Hola mundo AuthCustom</label>
+        {/* <img src={fondoHome} /> */}
+        <img className="portada"
+            alt="..."
+            src={require("assets/img/icons/home/diseño.png")}
+        />
         {props.children}
     </div>;
 }
--- pages/auth/home.js
+++ pages/auth/home.js
@@ -2,20 +2,46 @@ import "assets/css/home.css"
 import AuthCustom from "layouts/AuthCustom";
 function Home() {
     return <>
-        <img
-            alt="..."
-            src={require("assets/img/icons/home/IMG1.png")} class="img1" />
-        <img
-            alt="..."
-            src={require("assets/img/icons/home/IMG2.png")} class="img2" />
-        <img
-            alt="..."
-            src={require("assets/img/icons/home/IMG3.png")} class="img3" />
-        <img
-            alt="..."
-            src={require("assets/img/icons/home/IMG4.png")} class="img4" />
-        <h3>CONCYTEC</h3>
-        <h2>Iniciar sesión</h2></>
+        <div class="header-home">
+            <img
+                alt="..."
+                src={require("assets/img/icons/common/SIGECCYT.png")} class="logo" />
+            <img
+                alt="..."
+                src={require("assets/img/icons/common/club.png")} class="club" />
+            <img
+                alt="..."
+                src={require("assets/img/icons/common/concytec.png")} class="concytec" />
+        </div>
+        <div class="conteiner-title">
+            <h1>¡Bienvenid@!</h1>
+            <p class="title-p">Al sistema Integral de Gestión de clubes de ciencia y tecnología (SIGECCYT) del CONCYTEC, que tiene como finalidad pública brindar información detallada a los usuarios responsables de la implementación de los clubes a nivel nacional. </p>
+        </div>
+        <div class="img1">
+            <img
+                alt="..."
+                src={require("assets/img/icons/home/IMG1.png")} />
+            <h3>CCYT</h3>
+        </div>
+        <div class="img2">
+            <img
+                alt="..."
+                src={require("assets/img/icons/home/IMG2.png")}/>
+            <h3>UGEL</h3>
+        </div>
+        <div class="img3">
+            <img
+                alt="..."
+                src={require("assets/img/icons/home/IMG3.png")}/>
+            <h3>DRE/GRE</h3>
+        </div>
+        <div class="img4">
+            <img
+                alt="..."
+                src={require("assets/img/icons/home/IMG4.png")}/>
+            <h3>CONCYTEC</h3>
+        </div>
+    </>
 }
 Home.layout = AuthCustom;
 export default Home;

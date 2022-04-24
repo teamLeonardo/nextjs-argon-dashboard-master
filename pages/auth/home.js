3c977ec529d8a82693fc145899bfa3dea1febaf6
leonardo sifuentes <leonardosm3.14@gmail.com>
2022-04-24 17:44:53 -0500

    fix: correcion estilos

--- pages/auth/home.js
+++ pages/auth/home.js
@@ -13,34 +13,39 @@ function Home() {
                 alt="..."
                 src={require("assets/img/icons/common/concytec.png")} class="concytec" />
         </div>
-        <div class="conteiner-title">
-            <h1>¡Bienvenid@!</h1>
-            <p class="title-p">Al sistema Integral de Gestión de clubes de ciencia y tecnología (SIGECCYT) del CONCYTEC, que tiene como finalidad pública brindar información detallada a los usuarios responsables de la implementación de los clubes a nivel nacional. </p>
+        <div className="conteiner-page-home">
+            <div class="conteiner-title">
+                <h1>¡Bienvenid@!</h1>
+                <p class="title-p">Al sistema Integral de Gestión de clubes de ciencia y tecnología (SIGECCYT) del CONCYTEC, que tiene como finalidad pública brindar información detallada a los usuarios responsables de la implementación de los clubes a nivel nacional. </p>
+            </div>
+            <div className="groud-card-img">
+                <div class="img1">
+                    <img
+                        alt="..."
+                        src={require("assets/img/icons/home/IMG1.png")} />
+                    <h3>CCYT</h3>
+                </div>
+                <div class="img2">
+                    <img
+                        alt="..."
+                        src={require("assets/img/icons/home/IMG2.png")} />
+                    <h3>UGEL</h3>
+                </div>
+                <div class="img3">
+                    <img
+                        alt="..."
+                        src={require("assets/img/icons/home/IMG3.png")} />
+                    <h3>DRE/GRE</h3>
+                </div>
+                <div class="img4">
+                    <img
+                        alt="..."
+                        src={require("assets/img/icons/home/IMG4.png")} />
+                    <h3>CONCYTEC</h3>
+                </div>
+            </div>
         </div>
-        <div class="img1">
-            <img
-                alt="..."
-                src={require("assets/img/icons/home/IMG1.png")} />
-            <h3>CCYT</h3>
-        </div>
-        <div class="img2">
-            <img
-                alt="..."
-                src={require("assets/img/icons/home/IMG2.png")}/>
-            <h3>UGEL</h3>
-        </div>
-        <div class="img3">
-            <img
-                alt="..."
-                src={require("assets/img/icons/home/IMG3.png")}/>
-            <h3>DRE/GRE</h3>
-        </div>
-        <div class="img4">
-            <img
-                alt="..."
-                src={require("assets/img/icons/home/IMG4.png")}/>
-            <h3>CONCYTEC</h3>
-        </div>
+
     </>
 }
 Home.layout = AuthCustom;
